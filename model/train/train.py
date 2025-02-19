7f7ba248f83932fdc60feec8b7c96255ecd48fa2
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-02-19 16:44:50 +0700

    test

--- model/train/train.py
+++ model/train/train.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
-from config import _constants
+# from config import _constants
 
 
 def train():
     model = YOLO("yolo11n.pt")
 
-    data_path = _constants.DATA_PATH
+    # data_path = _constants.DATA_PATH
+    data_path = "./traffic-intelligent/model/datasets/Vehicle-Registration-Plates-1/data.yaml"
 
     train_results = model.train(
         data=data_path, epochs=200, device=2, lr0=0.0015, optimizer="SGD", patience=50
