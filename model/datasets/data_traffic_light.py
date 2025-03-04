d681206f0c89d059f30533797762b6387c63267d
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-04 16:42:40 +0700

    update data

--- model/datasets/data_traffic_light.py
+++ model/datasets/data_traffic_light.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
-
 rf = Roboflow(api_key="l6Ho8j3l6wmLGp96t3xP")
-project = rf.workspace("traficlightdetection").project("trafic-light-detection-wi5z8")
+project = rf.workspace("traficlightdetection").project("v5-j3ysc-r2yns")
 version = project.version(1)
 dataset = version.download("yolov11")
+                
--- model/train/train.py
+++ model/train/train.py
@@ -5,12 +5,12 @@ from config import _constants
 def train():
     model = YOLO("yolo11n.pt")
 
-    data_path = _constants.DATA_PATH
+    data_path = _constants.DATA_PATH_LIGHT
     # data_path = "./traffic-intelligent/model/datasets/Vehicle-Registration-Plates-1/data.yaml"
-    project = _constants.CHECKPOINT_PATH
+    project = _constants.CHECKPOINT_PATH_1
 
     train_results = model.train(
-        data=data_path, epochs=100, device=3, lr0=0.00015, optimizer="SGD", patience=50, project=project,
+        data=data_path, epochs=200, device=3, lr0=0.0015, optimizer="SGD", patience=50, project=project,
     )
 
     return train_results
