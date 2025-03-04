6132d562a6c8a050d7a9beaec60d6f0cedca345e
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-04 16:46:28 +0700

    up train traffic light

--- config/_constants.py
+++ config/_constants.py
@@ -4,6 +4,6 @@ DATA_PATH = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/da
 CHECKPOINT_PATH = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/train/checkpoints_test"
 
 
-DATA_PATH_LIGHT = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/datasets/trafic-light-detection--1/data.yaml"
+DATA_PATH_LIGHT = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/datasets/v5-1/data.yaml"
 
 CHECKPOINT_PATH_1 = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/train/checkpoints_lights"
