c55ab699fff3dbc4bc37047188430155b4b2c803
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-04 15:59:50 +0700

    fix

--- model/train/train.py
+++ model/train/train.py
@@ -10,7 +10,7 @@ def train():
     project = _constants.CHECKPOINT_PATH_1
 
     train_results = model.train(
-        data=data_path, epochs=200, device=3, lr0=0.0015, optimizer="SGD", patience=50, project=project,
+        data=data_path, epochs=100, device=3, lr0=0.00015, optimizer="SGD", patience=50, project=project,
     )
 
     return train_results
