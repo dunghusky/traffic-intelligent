8243746a1197d80fc48a96965fa702cd398f8e50
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-04 15:38:33 +0700

    up train traffic light

--- config/_constants.py
+++ config/_constants.py
@@ -2,3 +2,8 @@
 DATA_PATH = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/datasets/Vehicle-Registration-Plates-2/data.yaml"
 
 CHECKPOINT_PATH = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/train/checkpoints"
+
+
+DATA_PATH_LIGHT = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/datasets/trafic-light-detection--1/data.yaml"
+
+CHECKPOINT_PATH_1 = "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/train/checkpoints_lights"
--- model/train/train.py
+++ model/train/train.py
@@ -5,9 +5,9 @@ from config import _constants
 def train():
     model = YOLO("yolo11n.pt")
 
-    data_path = _constants.DATA_PATH
+    data_path = _constants.DATA_PATH_LIGHT
     # data_path = "./traffic-intelligent/model/datasets/Vehicle-Registration-Plates-1/data.yaml"
-    project = _constants.CHECKPOINT_PATH
+    project = _constants.CHECKPOINT_PATH_1
 
     train_results = model.train(
         data=data_path, epochs=200, device=2, lr0=0.0015, optimizer="SGD", patience=50, project=project,
