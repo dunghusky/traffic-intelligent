5072d8d4eb1af49c9c9572ba6dbc127f1fd58940
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-12 15:54:48 +0700

    test bug

--- test_4.py
+++ test_4.py
@@ -10,7 +10,7 @@ model = YOLO(
     task="detect",
 )
 
-result = model.track("https://ultralytics.com/images/bus.jpg", device=3)
+result = model.predict("https://ultralytics.com/images/bus.jpg")
 detections = sv.Detections.from_ultralytics(result)
 print("\nresult: ", result)
 print("\ndetections: ", detections)
