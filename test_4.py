a438751fac10787537316780c235607bf72c447c
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-12 15:51:54 +0700

    test bug

--- test_4.py
+++ test_4.py
@@ -8,9 +8,9 @@ print("Thiết bị đang được sử dụng:", device)
 model = YOLO(
     "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/model_export/model_tensorRT/yolo11n.engine",
     task="detect",
-).to(device)
+)
 
-result = model.predict("https://ultralytics.com/images/bus.jpg")
+result = model.track("https://ultralytics.com/images/bus.jpg", device=2)
 detections = sv.Detections.from_ultralytics(result)
 print("\nresult: ", result)
 print("\ndetections: ", detections)
