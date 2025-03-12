3f9f929c9097ac6af357b1e93beafcefee120017
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-12 16:03:22 +0700

    test bug

--- test_4.py
+++ test_4.py
@@ -7,10 +7,9 @@ device = "cuda:2" if torch.cuda.is_available() else "cpu"
 print("Thiết bị đang được sử dụng:", device)
 model = YOLO(
     "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/model_export/model_tensorRT/yolo11n.engine",
-    task="detect",
 )
 
-result = model.track("https://ultralytics.com/images/bus.jpg")
+result = model.predict("https://ultralytics.com/images/bus.jpg")
 detections = sv.Detections.from_ultralytics(result)
 print("\nresult: ", result)
 print("\ndetections: ", detections)
