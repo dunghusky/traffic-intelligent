75caf4e59183ad4f04f622bef233a25d92567fc1
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-03-12 15:47:54 +0700

    test bug

--- test_4.py
+++ test_4.py
@@ -1,11 +1,14 @@
+import torch
 from ultralytics import YOLO
 from config import _constants
 import supervision as sv
 
+device = "cuda:2" if torch.cuda.is_available() else "cpu"
+print("Thiết bị đang được sử dụng:", device)
 model = YOLO(
     "/home/ubuntu/mekongai/test_intelligent/traffic-intelligent/model/model_export/model_tensorRT/yolo11n.engine",
     task="detect",
-)
+).to(device)
 
 result = model.track("https://ultralytics.com/images/bus.jpg")
 detections = sv.Detections.from_ultralytics(result)
